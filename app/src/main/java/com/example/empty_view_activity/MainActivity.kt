d9e32349d453352997d23f74acf23246789422b6
vladgamerr <smirnov99vlad@gmail.com>
2024-11-10 20:37:30 +0300

    + plaska

--- app/src/main/java/com/example/empty_view_activity/MainActivity.kt
+++ app/src/main/java/com/example/empty_view_activity/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.empty_view_activity
 
 import FragmentNavigator
 import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.empty_view_activity.databinding.ActivityMainBinding
+import com.google.android.material.internal.EdgeToEdgeUtils
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -14,9 +17,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         // Инициализация FragmentNavigator
         fragmentNavigator = FragmentNavigator(this)
 
