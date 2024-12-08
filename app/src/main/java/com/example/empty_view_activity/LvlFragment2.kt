409769214c39660879151dbcadcfa11f99abd43a
Andrei Sheverinov <jobboxsh@mail.ru>
2024-12-08 21:50:10 +0300

    commit 3

--- app/src/main/java/com/example/empty_view_activity/LvlFragment2.kt
+++ app/src/main/java/com/example/empty_view_activity/LvlFragment2.kt
@@ -1,33 +1,78 @@
 package com.example.empty_view_activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.example.empty_view_activity.databinding.FragmentLvl2Binding
+import java.io.File
+import java.io.FileOutputStream
 
 
 class LvlFragment2 : Fragment() {
+    private lateinit var binding: FragmentLvl2Binding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        return inflater.inflate(R.layout.fragment_lvl2, container, false)
+        binding = FragmentLvl2Binding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.taskCardView.setOnClickListener {
+            binding.toShow.visibility = View.VISIBLE
+            binding.toHide.visibility = View.GONE
+        }
+        binding.presentationButton.setOnClickListener{
+            openPresentation()
+        }
 
+    }
+
+    private fun openPresentation() {
+        // Копируем файл из raw в кеш
+        val inputStream = resources.openRawResource(R.raw.my_presentation)
+        val tempFile = File(requireContext().cacheDir, "presentation.pptx")
+
+        inputStream.use { input ->
+            FileOutputStream(tempFile).use { output ->
+                input.copyTo(output)
+            }
+        }
+
+        // Генерируем URI с помощью FileProvider
+        val uri: Uri = FileProvider.getUriForFile(
+            requireContext(),
+            "${requireContext().packageName}.provider",
+            tempFile
+        )
+
+        // Создаем Intent для открытия файла
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        intent.setType("application/vnd.openxmlformats-officedocument.presentationml.presentation");
+
+        // Запускаем Intent с выбором приложения
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(intent)
+        } else {
+            Toast.makeText(context, "Нет приложений для открытия презентации.", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
@@ -35,3 +80,4 @@ class LvlFragment2 : Fragment() {
         fun newInstance() = LvlFragment2()
     }
 }
+
